3fcc747a17d9873e873fe22f13a86e582170b729
HitPant <30971790+HitPant@users.noreply.github.com>
2020-08-09 19:35:13 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,13 +2,13 @@ import os
 from twilio.rest import Client
 
 def send_sms(x):
-    account_sid= "ACd2e235572270ce48dc952b1dafff57bd" #from twilio account
-    auth_token= "e13051f30d6641df7d90afcda7066fbb" #from twilio account
+    account_sid= "*********" #from twilio account
+    auth_token= "*************" #from twilio account
     client= Client(account_sid,auth_token)
 
     client.messages.create(
-        to= "+919869016399",
-        from_="+12058906507", #provided by twilio
-        body= f"Social distance violation count: {x}. Name: Hemant.P.Sakhrani" #message
+        to= "8988775659",
+        from_="+1256777898", #provided by twilio
+        body= f"Social distance violation count: {x}" #message
     )
     print("sms sent...")
